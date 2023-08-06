b66932fea5f7d9001cb8c42bb5de627fb72a3660
yongbinlee <yblee001223@naver.com>
2023-08-06 08:01:07 +0000

    Add turtle_control package

--- yongbin/ros2_ws/build/test_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/yongbin/Repository/incsl_summer_2023/yongbin/ros2_ws/install/test_pubsub'
--- yongbin/ros2_ws/install/turtle_control/lib/python3.8/site-packages/turtle_control/teleop_key.py
+++
@@ -1,3 +0,0 @@
-from math import cos
-
-print(cos(20))
